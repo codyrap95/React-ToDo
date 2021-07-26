10f96e4140534a618819b803a3061a1725e47107
Cody <codydjay@gmail.com>
2021-07-26 20:58:27 +0300

    Converted app.module.css to css

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Header from "./components/UI/Header";
 import { ToDosContextProvider } from "./store/ToDosContext";
 import ToDoList from "./components/ToDoList";
 import AddToDo from "./components/AddToDo";
-import classes from "./App.module.css";
+import "./App.css";
 import Card from "./components/UI/Card";
 
 function App() {
   return (
     <ToDosContextProvider>
       <Header></Header>
-      <main className={classes.main}>
+      <main className="main">
         <Card>
           <AddToDo />
         </Card>
